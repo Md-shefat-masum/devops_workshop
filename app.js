84c3f24c667ee64cd3a0b4f1a884b317c63df10b
masum shefat <mshefat924@gmail.com>
2024-12-15 15:31:37 +0600

    version 2 network

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mysql = require('mysql2/promise');
 
-mongoose.connect('mongodb://98.81.55.100:27017/workshop_devops');
+// mongoose.connect('mongodb://98.81.55.100:27017/workshop_devops');
+mongoose.connect('mongodb://basic_node_app_network:27017/workshop_devops');
 
 const app = express();
 app.use(express.json());
 
 const dataSchema = new mongoose.Schema({ name: String });
 const Data = mongoose.model('User', dataSchema);
+
+app.get('/', async (req, res) => {
+    res.send('Hello World!');
+});
 
 app.post('/mongo', async (req, res) => {
     const data = new Data(req.body);
@@ -43,8 +48,8 @@ mysql.createConnection({ host: '98.81.55.100', user: 'remote_user', password: 'D
                 });
         });
 
-        app.listen(5500, '0.0.0.0', () => {
-            console.log('Server started on port http://98.81.55.100:5500');
+        app.listen(6001, '0.0.0.0', () => {
+            console.log('Server started on port http://98.81.55.100:6001');
         });
     })
     .catch(err => {
@@ -53,3 +58,5 @@ mysql.createConnection({ host: '98.81.55.100', user: 'remote_user', password: 'D
 
 
 /** scp -i workshop_2_aws_pkey.pem app.zip ubuntu@98.81.55.100:/var/www/node_app */
+
+//  docker run --name basic_node_app_mongo_db_container --network basic_node_app_network -p 28017:27017 -d mongo
