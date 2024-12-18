f25bc4d26b9044042e83e8df060ab4ca00a890bf
masum shefat <mshefat924@gmail.com>
2024-12-18 17:13:57 +0600

    docker cicd practice

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/host', function () {
-    dd(request()->getHost());
+    dd($_SERVER);
 });
 
 Route::get('/', function () {
